1be8f333b9ca42126c592705c9e0caa71090a25f
Riccardo939238 <riccardogiorio.1@gmail.com>
2025-05-25 11:14:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/api/product', async (req, res) => {
     const image = $('.woocommerce-product-gallery__image img').attr('src');
 
     // Prende solo il primo prezzo valido
-const currencySymbol = $('.summary .price .woocommerce-Price-currencySymbol').first();
+const price = $('.summary .price .woocommerce-Price-amount').first().text().trim();
 const price = currencySymbol.parent().text().replace(/\\s+/g, '').trim(); // rimuove spazi e restituisce "€274,00"
 
 
